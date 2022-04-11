8608dddc18ff9a883f2ff7fc70e694b4f70cde9b
Emilio Hernandez <emilio.hernandez@PLUSTI.COM>
2022-04-11 16:10:10 -0600

    Se agregan pantallas de home y login

---
+++ lib/Screens/Welcome/Welcome.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text("¡Hola, bienvenido a EnlatadosMG!",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.mulish(
+                        textStyle: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30))),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Es un gusto tenerte por acá :) \n Para empezar, verifiquemos tu identidad",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey[700], fontSize: 15),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage('assets/man.png')),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.red),
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Text("Iniciar sesión",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 18)),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {},
+                    color: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Text("Registrarse",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white)),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
